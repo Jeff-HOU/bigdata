dea74a6df38eac902f2f62b7e79e97c01f133895
Jeff-HOU <1085363891@qq.com>
2017-06-28 23:19:45 +0800

    today submit: only 1 4 6 8 12 are used

--- work/feature_extract.py
+++ work/feature_extract.py
@@ -70,8 +70,8 @@ starget_x = np.delete(starget, [1], axis=1)									  # target's x axis of scale
 # |  _________|    ______________|      ||
 # | |             |                     ||
 # 1 0.(x.max-x.min)/(t.max-t.min)
-## TODO: WHAT IS THIS????
-
+## NOTODO: WHAT IS THIS????
+'''
 utdata_delta_x = np.max(utdata_x, axis=1) - np.min(utdata_x, axis=1)
 utdata_delta_t = np.max(utdata_t, axis=1) - np.min(utdata_t, axis=1)
 avg_velocity_t = np.divide(utdata_delta_x,utdata_delta_t) # be care of overflow
@@ -81,6 +81,24 @@ usdata_delta_x = np.max(usdata_x, axis=1) - np.min(usdata_x, axis=1)
 usdata_delta_t = np.max(usdata_t, axis=1) - np.min(usdata_t, axis=1)
 avg_velocity_s = np.divide(usdata_delta_x,usdata_delta_t) # be care of overflow
 sfeature[:,0]= avg_velocity_s.reshape((1,100000))
+'''
+## replace original feature to the following:
+# 1 0. how many kinds of x.delta
+'''
+utdata_x_diff=np.diff(utdata_x[:,:,0])
+test=np.zeros((3000,1))
+last_t_index=count_record_num(training_or_testing="t")
+for i in range (0,3000):
+	test[i]=np.unique(utdata_x_diff[i,0:last_t_index[i]]).size
+tfeature[:,0]=test[:,0]
+
+usdata_x_diff=np.diff(usdata_x[:,:,0])
+test=np.zeros((100000,1))
+last_t_index=count_record_num(training_or_testing="s")
+for i in range (0,100000):
+	test[i]=np.unique(usdata_x_diff[i,0:last_t_index[i]]).size
+sfeature[:,0]=test[:,0]
+'''
 
 # 2 1. x.end - x0
 
@@ -105,14 +123,15 @@ sfeature[:,1]=sdata_x_trans[range(100000),endpoint_s]-starget_x[:,0]		  #the res
 
 
 # 3 2.x.max-x.min
-
+'''
 tdata_delta_x = np.max(utdata_x, axis=1) - np.min(utdata_x, axis=1)
 tfeature[:, 2] = tdata_delta_x.reshape((1, 3000))
 
 sdata_delta_x = np.max(usdata_x, axis=1) - np.min(usdata_x, axis=1)
 sfeature[:, 2] = sdata_delta_x.reshape((1, 100000))
-
+'''
 # 4 3. mean of sigma|x-x0|^2
+'''
 count_record_tnum = count_record_num(training_or_testing="t")
 for i in range(3000):
 	sigma_ = 0
@@ -128,22 +147,23 @@ for i in range(100000):
 		#set x0 to 0 when no data, set x0 to target_x when there is data
 		sigma_ = sigma_ + abs(sdata_x[i, j, 0] - starget_x[i,0])**2 # no squeeze
 	sfeature[i, 3] = sigma_/count_record_snum[i] #mean of sigma
-
+'''
 
 # 5 13 a new feature, depicting how many same time-point that a data possess
+
 utdata_x_diff=np.diff(utdata_x[:,:,0])										  #np.diff calculate the adjacent difference
 utdata_t_diff=np.diff(utdata_t[:,:,0])
 velocity_t=utdata_x_diff/utdata_t_diff										  #result in a number of nan
 number_of_same_time_t=np.sum((np.isinf(velocity_t)),axis=1)					  # a (3000,) data max is 144, min if 0 there are 574 entries that has same t in total
 #index_of_same_time_t=np.where(number_of_same_time_t>0)
-tfeature[:,13]=number_of_same_time_t
+#tfeature[:,13]=number_of_same_time_t
 
 usdata_x_diff=np.diff(usdata_x[:,:,0])										  #np.diff calculate the adjacent difference
 usdata_t_diff=np.diff(usdata_t[:,:,0])
 velocity_s=usdata_x_diff/usdata_t_diff										  #result in a number of nan
 number_of_same_time_s=np.sum((np.isinf(velocity_s)),axis=1)					  # a (10000,) data max is 241, min if 0 there are 14777 entries that has same t in total
 #index_of_same_time_t=np.where(number_of_same_time_t>0)
-sfeature[:,13]=number_of_same_time_s
+#sfeature[:,13]=number_of_same_time_s
 
 # 5 14 the mean of velocity
 
@@ -151,13 +171,14 @@ velocity_t_copy=np.copy(velocity_t)
 velocity_t_copy[np.isinf(velocity_t_copy)]=300								  #stipulate that the speed of inf equals 300 for inf
 velocity_t_copy[velocity_t_copy>300]=300
 velocity_t_mean=np.nanmean(np.absolute(velocity_t_copy),axis=1)				  #no scaling because velocity is too big for some, resulting in infinitisimal for the normal one
-tfeature[:,14]=velocity_t_mean												  #/(np.amax(velocity_t_mean)-np.amin(velocity_t_mean)) this i for scaling
+#tfeature[:,14]=velocity_t_mean												  #/(np.amax(velocity_t_mean)-np.amin(velocity_t_mean)) this i for scaling
 
 velocity_s_copy=np.copy(velocity_s)
 velocity_s_copy[np.isinf(velocity_s_copy)]=300								  #stipulate that the speed of inf equals 300 for inf
 velocity_s_copy[velocity_s_copy>300]=300									  #a strange case with max 136900 is discovered, probably the t is too small
 velocity_s_mean=np.nanmean(np.absolute(velocity_s_copy),axis=1)				  #no scaling because velocity is too big for some, resulting in infinitisimal for the normal one
-sfeature[:,14]=velocity_s_mean												  #/(np.amax(velocity_s_mean)-np.amin(velocity_s_mean)) this is for scaling
+
+#sfeature[:,14]=velocity_s_mean												  #/(np.amax(velocity_s_mean)-np.amin(velocity_s_mean)) this is for scaling
 
 # 5 4. the standard deviation of velocity
 
@@ -169,7 +190,7 @@ sfeature[:,4]=velocity_s_std
 
 
 # 5 15.|xi-xi+1|/|ti-ti+1|^2 mean of acceleration
-
+'''
 acceleration_t=velocity_t/utdata_t_diff
 acceleration_t[np.isinf(acceleration_t)]=50
 acceleration_t[acceleration_t>100]=50
@@ -179,11 +200,12 @@ acceleration_s=velocity_s/usdata_t_diff
 acceleration_s[np.isinf(acceleration_s)]=50
 acceleration_s[acceleration_s>100]=50
 sfeature[:,15]=np.nanmean(np.absolute(acceleration_s),axis=1)
-
+'''
 #5 5. the standard deviation of accelration
+'''
 tfeature[:,5]=np.nanstd(acceleration_t,axis=1)
 sfeature[:,5]=np.nanstd(acceleration_s,axis=1)
-
+'''
 # 6 6.Stop times:
 tdata_x_no = np.squeeze(tdata_x, axis=2)
 tdata_x_no1 = np.delete(tdata_x_no, [0], axis=1) # shape: (3000, 299)
@@ -239,6 +261,7 @@ for i in range(100000):
 
 
 # 7 7.Any fluctuation when stopping The continuous 5 x_diff is 0: mean ratio of no_fluct_num++ OR fluctuation of slope
+'''
 record_num_t = count_record_num(training_or_testing="t")
 tdata_x_diff=np.diff(tdata_x[:,:,0])
 tdata_t_diff=np.diff(tdata_t[:,:,0])
@@ -268,7 +291,7 @@ for i in range(100000):
 		if (abs(sdata_k[i,j]) + abs(sdata_k[i,j+1]) + abs(sdata_k[i,j+2]) + abs(sdata_k[i,j+3]) + abs(sdata_k[i,j+4])) == 0 : #all 0 - the threshold waits for tuning
 			no_fluct_num_s = no_fluct_num_s + 1
 	sfeature[i,7] = no_fluct_num_s/(record_num_s[i]/5)
-
+'''
 
 #record_num_t = count_record_num(training_or_testing="t")
 #tdata_x_diff=np.diff(tdata_x[:,:,0])
@@ -297,6 +320,7 @@ for i in range(100000):
 #	sfeature[i,7] = no_fluct_num_s/(record_num_s[i]-5)
 
 # 8 8.Back distance:
+
 utdata_x_trans=utdata_x[:,:,0]
 utdata_x_route_length=np.sum(np.absolute(utdata_x_trans),1)                         #the overall route length of the mouse
 utdata_x_diff_copy=np.diff(utdata_x_trans).copy()
@@ -326,7 +350,7 @@ for i in range(3000):
 		poly_size = int(np.floor(num_of_nonzeros / 20 + 5))
 	utdata_xss[i, 0: num_of_nonzeros] = savitzky_golay(utdata_xs[i, 0: num_of_nonzeros], window_size, poly_size)
 '''
-
+'''
 utdata_s = np.copy(utdata)
 for i in range(3000):
 	num_of_nonzeros = np.count_nonzero(utdata_s[i, :, 0])
@@ -364,8 +388,9 @@ ssmooth_x_mse = (ssmooth_x ** 2).mean(axis=1) # SOME NAN AFTER THIS STEP!!!!
 											# np.count_nonzero(~np.isnan((ssmooth_x_mse))) --> NON-NAN: 2924
 sfeature[:, 9] = np.mean(ssmooth_x, axis=1).reshape((1, 100000))
 sfeature[:, 10] = ssmooth_x_mse.reshape((1, 100000))
-
+'''
 # 10 11.if x<x0, sum of all t_diff when x unchange - diff && count_inf OR 0/1 mask
+'''
 record_num_t = count_record_num(training_or_testing="t")
 utdata_x_diff=np.diff(utdata_x[:,:,0])
 utdata_t_diff=np.diff(utdata_t[:,:,0])
@@ -391,7 +416,7 @@ for i in range(100000):
 		if bool_same_x_s[i,j]==1 and usdata_x[i,j,0] < ustarget_x[i,0]:
 			sum_time_unchange = sum_time_unchange + usdata_t_diff[i,j]
 	sfeature[i,11] = sum_time_unchange
-
+'''
 # 11 12. judging the similarity of the line with a straight line
 #        there are two cases, one with a direct line, another with x remaining the same first and then followed by a direct sloping line
 #        Then it is natural to focus on the regression of the direct line only.
